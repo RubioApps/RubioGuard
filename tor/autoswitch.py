e7efe9a3f9af81689a764989c5282f617d5c1f00
Jaime Rubio <155658204+RubioApps@users.noreply.github.com>
2024-01-12 09:14:10 +0100

    Create autoswitch.py

---
+++ tor/autoswitch.py
@@ -0,0 +1,39 @@
+import requests
+import time
+import logging
+import json
+from systemd.journal import JournalHandler
+from stem import Signal
+from stem.control import Controller
+
+logger = logging.getLogger(__name__)
+handler = JournalHandler(SYSLOG_IDENTIFIER='tor')
+handler.setFormatter(logging.Formatter('%(message)s'))
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+def get_current_ip():
+	session = requests.session()
+
+    # TO Request URL with SOCKS over TOR
+	session.proxies = {}
+	session.proxies['http']='socks5h://localhost:9050'
+	session.proxies['https']='socks5h://localhost:9050'
+
+	try:
+		r = session.get('http://httpbin.org/ip')
+	except Exception as e:
+		return str(e)
+	else:
+		jsonData = json.loads(r.text)
+		return 'Switch the exit node. New origin: ' + jsonData['origin']
+
+def renew_tor_ip():
+
+	with Controller.from_port(port = 9051) as controller:
+		controller.authenticate(password="M1l4n4@B0n1t4")
+		controller.signal(Signal.NEWNYM)
+
+if __name__ == "__main__":
+	renew_tor_ip()
+	logger.info('%s', get_current_ip())
